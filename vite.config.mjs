fcb33295e28bbb1406e8d8a9b605d2dde05558cb
Kristian Goystdal <krisgoy100@gmail.com>
2025-05-09 00:50:42 +0200

    fix: update base path for deployment in Vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Card_Chess_Game/',
+  base: '/card_chess/',
   plugins: [
     VueRouter(),
     Vue({
