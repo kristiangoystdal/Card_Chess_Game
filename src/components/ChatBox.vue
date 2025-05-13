a9d75363bb92db96be5652ec566e244b94e003c5
Kristian Gøystdal <krisgoy100@gmail.com>
2025-05-13 20:28:27 +0200

    feat: add ChatBox component for displaying chat messages

---
+++ src/components/ChatBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row>
+    Chat messages will be displayed here.
+    <v-col cols="12">
+      <v-text-field label="Chat is currently disabled... Stay tuned" outlined></v-text-field>
+      <v-btn color="primary" :disabled="true">Send</v-btn>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12">
+      <p><strong>Player 1:</strong> Hello!</p>
+      <p><strong>Player 2:</strong> Hi there!</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "ChatBox",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+
+</script>
